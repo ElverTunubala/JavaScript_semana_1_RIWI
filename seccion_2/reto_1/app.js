f86c98e8f7e30db23a01cecaeac536418b2cb8c6
daguttt <dagutmu667@gmail.com>
2024-04-18 15:36:28 -0500

    challenge two was added

--- seccion_2/reto_1/app.js
+++ seccion_2/reto_1/app.js
@@ -1,14 +1,22 @@
 
 let nivelEnergia=prompt("del 0 al 5 como te sientes de energia hoy?")
 let clima = prompt("como esta el clima hoy, frio, soleado, lluvioso?")
-let cargaAcademica= prompt("tienes muchos trabajos?")
+let cargaAcademica= prompt("tienes muchos trabajos SI O N0?")
+
+if(cargaAcademica=="no" | cargaAcademica=="NO"){
+    cargaAcademica=false
+    console.log("puedes hacer otras cosas"+cargaAcademica)
+}else{
+    console.log("ovio que es un true por que es string")
+}
 
 let prueba = Boolean(nivelEnergia)
 
-if (prueba=="false"){
+if (prueba==false){
     console.log("le recomiendo tomar un dia libre")
-}else{
-    console.log("al menos entra al div")
+
+}else if(nivelEnergia== "0" | nivelEnergia=="1" | nivelEnergia=="2" | nivelEnergia=="3"){
+    console.log("LE RECOMIENDO TOMAR UN DIA LIBRE")
 }
 
 console.log(prueba)
---
+++ seccion_2/reto_2/app.js
@@ -0,0 +1,30 @@
+
+let presupuestoDiario = parseFloat(prompt("Ingrese su presupuesto diario:"));
+let costoComida = parseFloat(prompt("Ingrese el costo estimado de comer fuera:"));
+let costoLibro = parseFloat(prompt("Ingrese el costo estimado de comprar un libro:"));
+let objetivoAhorroDiario = parseFloat(prompt("Ingrese su objetivo de ahorro diario:"));
+
+// Evaluacion de los gastos
+let puedeComerFuera = presupuestoDiario >= costoComida;
+let puedeComprarLibro = presupuestoDiario >= costoLibro;
+let puedeAhorrar = presupuestoDiario - costoComida - costoLibro >= objetivoAhorroDiario;
+
+// imprimir los resultados
+if (puedeComerFuera && puedeComprarLibro && puedeAhorrar) {
+    console.log("¡Puedes comer fuera y comprar un libro y tambien ahorrar!");
+
+} else if (puedeComerFuera && puedeAhorrar) {
+    console.log("Puedes comer fuera y tambien ahorrar");
+
+} else if (puedeComprarLibro && puedeAhorrar) {
+    console.log("Puedes comprar un tambien ahorrar");
+
+} else if (puedeComerFuera) {
+    console.log("Puedes comer fuera, pero asegúrate de no pasarte de tu objetivo de ahorro.");
+
+} else if (puedeComprarLibro) {
+    console.log("Puedes comprar un libro, pero asegúrate de no pasarte de tu objetivo de ahorro.");
+
+} else {
+    console.log("Lo siento, no puedes  ni comer fuera, ni comprar un libro mientras alcanzas ese objetivo de ahorro.");
+}
